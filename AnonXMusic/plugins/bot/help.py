19306aad40c28e6ba2c02c0e776133fa277f4b97
˹sᴜᴍɪᴛ ✗ ˼ <118965746+SUMIT0481@users.noreply.github.com>
2023-09-13 23:53:53 +0530

    Update help.py
    Signed-off-by: ˹sᴜᴍɪᴛ ✗ ˼ <118965746+SUMIT0481@users.noreply.github.com>

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -38,8 +38,8 @@ async def helper_private(
         language = await get_lang(update.chat.id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_photo(
-            photo=START_IMG_URL,
+        await update.reply_animation(
+            animation=START_IMG_URL,
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
         )
